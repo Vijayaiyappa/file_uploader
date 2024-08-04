3bcc9d76d08a5e0d32270f3f0ec1df9718da6cc6
Vijayaiyappa <vijayaiyapparock@gmail.com>
2024-08-04 17:18:14 +0100

    commit

--- file_uploader/gcs_uploader.py
+++ file_uploader/gcs_uploader.py
@@ -1,7 +1,11 @@
 from google.cloud import storage
 import os
+from pathlib import Path
 
 def upload_to_gcs(file_path, bucket_name, project, credentials):
+    p1=Path(__file__).parents[1] 
+    credentials = Path(f'{p1}\//{credentials}')
+    # print("cred",credentials)
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials
     client = storage.Client(project=project)
     bucket = client.bucket(bucket_name)
